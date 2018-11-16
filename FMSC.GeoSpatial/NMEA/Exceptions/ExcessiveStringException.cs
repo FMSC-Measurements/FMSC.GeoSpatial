8a3f08bb2c2a97917dc921a420443402034d46bd
Bryan Miller <bryancmiller@fs.fed.us>
2018-11-16 14:10:18 -0700

    Initial commit

---
+++ FMSC.GeoSpatial/NMEA/Exceptions/ExcessiveStringException.cs
@@ -0,0 +1,8 @@
+﻿namespace FMSC.GeoSpatial.NMEA.Exceptions
+{
+    public class ExcessiveStringException : NmeaException
+    {
+        public ExcessiveStringException(SentenceID sentenceID) :
+            base($"An Excessive amount of {sentenceID} type strings were added to the burst."){ }
+    }
+}
